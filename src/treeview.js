3ea637df086b9790ab575402dd4c3b923537cf06
etk66296 <d_heile@web.de>
2020-11-16 21:14:22 +0100

    new layout, login form

--- src/treeview.js
+++ src/treeview.js
@@ -12,3 +12,14 @@ requestTreeLayerData = function(url, onSuccess) {
   xmlhttp.open("GET", url, true)
   xmlhttp.send()
 }
+
+loginRequest = function() {
+  var xhttp = new XMLHttpRequest()
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       console.log("Hello Login")
+    }
+  }
+  xhttp.open("POST", "http://localhost:9999/login.php", true);
+  xhttp.send();
+}
